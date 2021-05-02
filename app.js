fd788d885404e368121e0b409115effeb54f64cd
marksavin <mark831@my.yorku.ca>
2021-05-02 17:02:06 -0400

    changes made

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let checked = JSON.parse(localStorage.getItem("checkbox1"));
 document.getElementById("checkbox1").checked = checked;
 
@@ -24,30 +23,3 @@ function save() {
   let checkbox = document.querySelector("#checkbox1");
   localStorage.setItem("checkbox1", checkbox.checked);
 }
-=======
-let checked = JSON.parse(localStorage.getItem("checkbox1"));
-document.getElementById("checkbox1").checked = checked;
-
-if (document.getElementById("checkbox1").checked === true) {
-  myFunction();
-}
-
-function myFunction() {
-  var element = document.getElementsByClassName("intro-blue");
-  for (let i = 0; i < element.length; i++) {
-    element[i].classList.toggle("intro-red");
-    console.log(element[i]);
-  }
-
-  var element = document.getElementsByClassName("intro-btn-blue");
-  for (let i = 0; i < element.length; i++) {
-    element[i].classList.toggle("intro-btn-red");
-    console.log(element[i]);
-  }
-}
-
-function save() {
-  let checkbox = document.querySelector("#checkbox1");
-  localStorage.setItem("checkbox1", checkbox.checked);
-}
->>>>>>> 11b12d6f1d009aaaa5bb3a146fdff8a0bb40373b
