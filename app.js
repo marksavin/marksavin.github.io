f03783d21ed9ee8b2d19ee811012f2455bfe626f
marksavin <mark831@my.yorku.ca>
2021-05-17 17:49:56 -0400

    burger menu fixed!

--- app.js
+++ app.js
@@ -23,3 +23,20 @@ function save() {
   let checkbox = document.querySelector("#checkbox1");
   localStorage.setItem("checkbox1", checkbox.checked);
 }
+
+// BURGER MENU
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle-menu");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle-menu");
+  });
+});
